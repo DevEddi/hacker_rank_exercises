e1f2261a9d811b73d5a9bfb1822bb34150779d06
Edmilson Silva <56802572+DevEddi@users.noreply.github.com>
2022-04-17 14:56:48 -0300

    Atividade if else Hacker Rank

---
+++ main.py
@@ -0,0 +1,25 @@
+
+n = 3
+
+if n % 2 == 0:
+    if n >= 2 and n <= 5:
+        print('Not Weird')
+    elif n >= 6 and n <= 20:
+        print('Weird')
+    else:
+        print('Not Weird')
+else:
+    print('Weird')
+
+n = n + 17
+
+if n % 2 == 0:
+    if n >= 2 and n <= 5:
+        print('Not Weird')
+    elif n >= 6 and n <= 20:
+        print('Weird')
+    else:
+        print('Not Weird')
+else:
+    print('Weird')
+
